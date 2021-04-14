85871cf3102747deec97c62ec097fefacbe06a47
Amir <gamirov13@mail.ru>
2021-04-14 22:49:43 +0300

    добавил сервер для магазина

---
+++ сервер магазина/сервер магазина/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace сервер_магазина
+{
+    class Product
+    {
+    }
+}
---
+++ сервер магазина/сервер магазина/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace сервер_магазина
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
